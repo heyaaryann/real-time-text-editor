38337f4482b35d62e8c7cd2bcb6249982091560b
heyaaryann <aryangoyal1031@gmail.com>
2025-09-12 14:12:40 +0530

    small updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 const Home = () => {
   return ( 
     <div className="flex min-h-screen items-center justify-center">
-      
-      Click<Link href="documents/123">
-      <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
-      </Link> to go to document Id
+
+      <div className="text-black italic">
+        WELCOME AND CLICK HERE TO GO TO ---<br />
+
+      </div>
+       
+      <Link href="documents/123">
+      <span className="text-blue-500 underline">&nbsp;Real-time-text-editor&nbsp;</span>
+      </Link> 
     </div>
    );
 }
